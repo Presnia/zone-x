dda72024e4f6ca8182541f5454b238ca57850d06
Andrei Dzebysh <presnia@yahoo.com>
2020-11-29 23:37:54 +0300

    In catalog.html added data-text to find by, fin with chckbxs cat-fil-tog

--- src/js/components/catalog-filter-toggle.js
+++ src/js/components/catalog-filter-toggle.js
@@ -12,6 +12,17 @@ vars.$hideFilters.addEventListener('click', (e) => {
   });
 });
 
+const createChoiceItem = (text) => {
+  return (
+    `
+    <button class="btn-reset catalog-choice__item">
+        ${text}
+        <img class="catalog-choice__item--img" src="../img/close.svg" alt="">
+      </button>
+    `
+  )
+}
+
 vars.$catalogFilterItems.forEach(el => {
     el.querySelector('input').addEventListener('change', (e) => {
       console.log(el.querySelector('input'))
@@ -19,6 +30,10 @@ vars.$catalogFilterItems.forEach(el => {
 
       if (checked) {
         el.querySelector('.custom-checkbox').classList.add('custom-checkbox--active');
+        let text = el.querySelector('.custom-checkbox__text').textContent;
+
+        vars.$catalogChoice.insertAdjacentHTML('afterbegin', createChoiceItem(text));
+
       } else {
         el.querySelector('.custom-checkbox').classList.remove('custom-checkbox--active');
       }
@@ -32,4 +47,34 @@ vars.$catalogFilterItems.forEach(el => {
         vars.$catalogChoice.style.display = 'none';
       }
     });
+  });
+
+  vars.$catalogChoice.addEventListener('click', (e) => {
+    if (e.target.classList.contains('catalog-choice__item')) {
+      e.target.remove();
+
+      let text = e.target.textContent.trimLeft().trimRight();
+
+      document.querySelector(`[data-text="${text}"]`).querySelector('input').checked = false;
+      document.querySelector(`[data-text="${text}"]`).classList.remove('custom-checkbox--active');
+    }
+
+    if (e.target.classList.contains('catalog-choice__clear')) {
+      Array.from(e.currentTarget.children).forEach(el => {
+        if (!el.classList.contains('catalog-choice__clear')) {
+          el.remove();
+        }
+        
+        vars.$catalogFilterItems.forEach(el => {
+          el.querySelector('input').checked = false;
+          el.querySelector('.custom-checkbox').classList.remove('custom-checkbox--active');
+        });
+      });
+
+      e.currentTarget.style.display = 'none';
+    }
+
+    if (e.currentTarget.children.length === 1) {
+      e.currentTarget.style.display = 'none';
+    }
   });
