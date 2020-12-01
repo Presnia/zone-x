7900ae720d358f0f1a1505abcacfdbada773da81
Andrei Dzebysh <presnia@yahoo.com>
2020-12-01 12:14:43 +0300

    Added buttons to slider in hero-catalog

--- src/js/components/catalog-slider.js
+++ src/js/components/catalog-slider.js
@@ -4,4 +4,8 @@ import vars from '../_vars';
 const catalogSlider = new Swiper (vars.$catalogSlider, {
   loop: true,
   slidesPerView: 1,
+  navigation: {
+    nextEl: '.hero-next-btn',
+    prevEl: '.hero-prev-btn',
+  },
 });
